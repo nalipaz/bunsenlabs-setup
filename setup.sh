f8e5cf3b7f74a77c669849d9822b92c3c4575616
Nicholas Alipaz <nicholas@alipaz.net>
2016-04-18 15:31:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ sudo apt-get install libxss1 libappindicator1 libindicator7 -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 ## Add to startups as a background app.
-echo "(sleep 4 && /usr/bin/google-chrome-stable --no-startup-window) &" >> .config/openbox/autostart
+echo "(sleep 4 && /usr/bin/google-chrome-stable --no-startup-window) &" >> /home/$USER/.config/openbox/autostart
 
 # vim and ag.
 sudo apt-get install vim silversearcher-ag -y
 
 # Shutter.
-mv bunsenlabs-setup/home/.shutter ~
+mv bunsenlabs-setup-master/home/.shutter ~
 sudo apt-get install shutter -y 
 
 # Virtualbox and virtualbox extension pack for usb support.
-sudo apt-get install build-essential linux-headers-`uname -r` dkms libxml-xpath-perl -y
+sudo apt-get install build-essential linux-headers-`uname -r` dkms libxml2-utils -y
 sudo apt-get install -t jessie-backports virtualbox -y
 curl -LO $(curl -s "https://www.virtualbox.org/wiki/Downloads" | xmllint --xpath 'string((//div[@id="wikipage"]//a[@class="ext-link"])[4]/@href)' --html /tmp/vbox.html -)
 VBoxManage extpack install --replace Oracle*.vbox-extpack
@@ -26,7 +26,7 @@ VBoxManage extpack install --replace Oracle*.vbox-extpack
 sudo sed -i 's@greeter\-hide\-users=true@greeter-hide-users=false@' /etc/lightdm/lightdm.conf
 
 # Setup plymouth for a better login experience in lvm encrypted drives.
-sudo aptitude install plymouth plymouth-themes
+sudo aptitude install plymouth plymouth-themes -y
 convert ~/Pictures/wallpapers/shared/bunsen/bunsen-images/bl-default/bl-login-1920x1200.png -resize $(xdpyinfo | echo $(grep 'dimensions:') | sed -E "s@dimensions:\s([0-9]+)x([0-9]+).*@\1x\2@")^ -gravity center -crop $(xdpyinfo | echo $(grep 'dimensions:') | sed -E "s@dimensions:\s([0-9]+)x([0-9]+).*@\1x\2@")+0+0 ~/Pictures/wallpapers/bl-grub-$(xdpyinfo | echo $(grep 'dimensions:') | sed -E "s@dimensions:\s([0-9]+)x([0-9]+).*@\1x\2@").png
 cp ~/Pictures/wallpapers/bl-grub-*.png ~/Pictures/wallpapers/bl-wallpaper.png
 sudo cp ~/Pictures/wallpapers/bl-grub-*.png /boot/grub/
@@ -49,7 +49,7 @@ sudo mv obmenu-generator/obmenu-generator /usr/bin/
 sudo cpanm Linux::DesktopFiles
 sudo cpanm Data::Dump
 rm obmenu-generator
-mv bunsenlabs-setup/home/.config/obmenu-generator ~/.config/
+mv bunsenlabs-setup-master/home/.config/obmenu-generator ~/.config/
 
 # Alter tint2 preferences.
 sed -r 's@^#time@time@' ~/.config/tint2/tint2rc
