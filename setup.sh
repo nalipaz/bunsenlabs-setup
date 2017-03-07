25a7db68337b5797b141b41e475410eae147c1a7
Nicholas Alipaz <nicholas@alipaz.net>
2017-03-06 16:34:47 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -159,7 +159,7 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/debian \
-$(lsb_release -cs) \
+jessie \
 stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
