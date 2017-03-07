367daadc00b0c483b7cf985d2bf806dd661c211e
Nicholas Alipaz <nicholas@alipaz.net>
2017-03-06 16:31:55 -0800

    docker

--- setup.sh
+++ setup.sh
@@ -156,6 +156,12 @@ sudo reboot
 
 # Setup Docker
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/debian \
+$(lsb_release -cs) \
+stable"
+sudo apt-get update
 sudo apt-get -y install docker-ce
 sudo mkdir -p /etc/systemd/system/docker.service.d/ &&\
 printf '[Service]
